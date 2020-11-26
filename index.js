e8eca3ce8078fc0428553fc6fd4961ef9f3b210e
Artur Trifonov <wrgraff@gmail.com>
2020-11-26 23:13:40 +0300

    add cyrillic support

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const message = atob(hash.replace('#', ''));
 if (message) {
     document.querySelector('#message-form').classList.add('hide');
     document.querySelector('#message-show').classList.remove('hide');
-    document.querySelector('#message-show h1').textContent = message;
+    document.querySelector('#message-show h1').textContent = fromBinary(message);
 }
 
 document.querySelector('form').addEventListener('submit', evt => {
@@ -14,9 +14,25 @@ document.querySelector('form').addEventListener('submit', evt => {
     document.querySelector('#link-form').classList.remove('hide');
 
     const inputElement = document.querySelector('#message');
-    const encrypted = btoa(inputElement.value);
+    const encrypted = btoa(toBinary(inputElement.value));
 
     const link = document.querySelector('#link');
     link.value = `${window.location}#${encrypted}`;
     link.select();
 });
+
+function toBinary(string) {
+    const codeUnits = new Uint16Array(string.length);
+    for (let i = 0; i < codeUnits.length; i++) {
+        codeUnits[i] = string.charCodeAt(i);
+    }
+    return String.fromCharCode(...new Uint8Array(codeUnits.buffer));
+}
+
+function fromBinary(binary) {
+    const bytes = new Uint8Array(binary.length);
+    for (let i = 0; i < bytes.length; i++) {
+        bytes[i] = binary.charCodeAt(i);
+    }
+    return String.fromCharCode(...new Uint16Array(bytes.buffer));
+}
